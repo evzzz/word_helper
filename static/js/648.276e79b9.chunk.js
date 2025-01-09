ccd3ea9d77902e31437bb3f8a49893a80f344ec7
evzzz <alexandr@evseev.ru>
2025-01-09 11:29:34 +0300

    Updates

---
+++ static/js/648.276e79b9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkword_helper=self.webpackChunkword_helper||[]).push([[648],{1648:(e,l,n)=>{n.r(l),n.d(l,{default:()=>s});n(5043);var t=n(8073),i=n(9481),r=n(579);const s=()=>(0,r.jsx)(t.A,{container:!0,justifyContent:"center",children:(0,r.jsx)(t.A,{item:!0,xs:11,children:(0,r.jsx)(i.A,{label:"\u0412\u0445\u043e\u0434",multiline:!0,minRows:40,fullWidth:!0,variant:"outlined"})})})}}]);
+//# sourceMappingURL=648.276e79b9.chunk.js.map
